93aec8e775721f95aa3f74c016cbc7da331247e2
Maarten Van Hoof <maarten@pixeo.be>
2019-02-11 19:56:27 +0100

    fix(color): provided correct Color output

--- index.js
+++ index.js
@@ -108,14 +108,14 @@ module.exports = function (options) {
             color: colorOptions.text,
             '&:focus': {
               outline: 0,
-              boxShadow: `0 0 0 .2em ${Color(colorOptions.background).alpha(.5)}`,
+              boxShadow: `0 0 0 .2em ${Color(colorOptions.background).alpha(.5).rgb().toString()}`,
             },
             '&:hover': {
-              backgroundColor: _.get(colorOptions, 'hoverBackground', Color(colorOptions.background).darken(0.1)),
+              backgroundColor: _.get(colorOptions, 'hoverBackground', Color(colorOptions.background).darken(0.1).hex().toString()),
               color: _.get(colorOptions, 'hoverText', colorOptions.text),
             },
             '&:active': {
-              backgroundColor: _.get(colorOptions, 'activeBackground', Color(colorOptions.background).darken(0.1)),
+              backgroundColor: _.get(colorOptions, 'activeBackground', Color(colorOptions.background).darken(0.1).hex().toString()),
               color: _.get(colorOptions, 'activeText', colorOptions.text),
             }
           }
