15dacb871c514d251bae9ac1bfe878aeccf51594
Maarten Van Hoof <maarten@pixeo.be>
2019-02-11 15:08:34 +0100

    feat(npm): added lockfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const _ = require('lodash')
-const Color = require('color')
-const defaultConfig = require('tailwindcss/defaultConfig')()
+const _ = require('lodash');
+const Color = require('color'):
+const defaultConfig = require('tailwindcss/defaultConfig')():
 
 function defaultOptions() {
   return {
